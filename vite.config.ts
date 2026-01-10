1c76a654da231922b857a94aa9e8e467968a53ab
DamianAntczak <d_antczak@op.pl>
2026-01-10 22:29:55 +0100

    configure plugin to generate sitemap.xml

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Sitemap from 'vite-plugin-sitemap';
+import {StatsType} from './src/common/StatsType.ts'
+import hospitals from './src/assets/hospitals.json';
 
-// https://vite.dev/config/
+let dynamicRoutes: string[] = [];
+hospitals.forEach((hospital) => {
+    Object.values(StatsType).forEach(type => {
+        dynamicRoutes.push(
+            '/birth-statistics-pl/#/hospital/' + hospital.value + '/statsType/' + type);
+    })
+})
+
 export default defineConfig({
-  base: 'birth-statistics-pl',
-  plugins: [vue()],
+    base: 'birth-statistics-pl',
+    plugins: [vue(),
+        Sitemap({
+            hostname: 'https://damianantczak.github.io/birth-statistics-pl',
+            dynamicRoutes: dynamicRoutes,
+            changefreq: 'weekly',
+            priority: 0.8
+        })
+    ]
 })
